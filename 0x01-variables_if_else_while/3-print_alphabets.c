ee33f4cae88b6408c8caacfd3d0515a5d0419a18
Happykhamissa <elmansourikhamissa@gmail.com>
2023-06-16 15:10:26 -0700

    Alphabete

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints alpha in lower case
+ * main - Prints alpha in lower and upper case
  * Return: Always 0
  */
 
 int main(void)
 {
-char lc;
-
-for (lc = 'a'; lc <= 'z'; lc++)
-{
-putchar(lc);
-}
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'A'; ch++)
+putchar(ch);
 putchar('\n');
 return (0);
 }
-
-
