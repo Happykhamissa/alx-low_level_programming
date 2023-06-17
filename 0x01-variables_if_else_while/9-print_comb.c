60543b7a11a001a3b4e58ce25ac159c5beb25121
Happykhamissa <elmansourikhamissa@gmail.com>
2023-06-17 02:43:15 -0700

    success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,39 @@
 int main(void)
 {
 int n;
-for (n = 48; n < 58; n++)
-putchar(n);
+for (n = 48; n < 58; n++
 if (n != 57)
 {
-putchar(', ');
+#include <stdio.h>
+
+/**
+ * main - Prints all combination of single digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++
+if (n != 57)
+{
+#include <stdio.h>
+
+/**
+ * main - Prints all combination of single digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+if (n != 57)
+{
+putchar(n);
+putchar(',');
 putchar(' ');
 }
-else
-{
 putchar('\n');
-}
 return (0);
 }
