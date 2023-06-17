b7052843a45553490e422d1ab7e78eb9fba00856
Happykhamissa <elmansourikhamissa@gmail.com>
2023-06-17 03:36:18 -0700

    success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 int n;
-for (n = 48; n < 59; n++)
-if (n != 58)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
-}
-if (n <= 57)
+if (n != 57)
 {
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
