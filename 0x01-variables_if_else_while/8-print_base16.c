435547bc9e67dea10b8b67a5195647eb197ba493
Happykhamissa <elmansourikhamissa@gmail.com>
2023-06-17 02:47:57 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 int num;
 for (num = '0'; num <= '9'; num++)
 putchar(num);
-for (num = 'a'; la <= 'f'; la++)
+for (num = 'a'; num <= 'f'; num++)
 putchar(num);
 putchar('\n');
 return (0);
