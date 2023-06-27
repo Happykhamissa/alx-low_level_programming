2c3b70cb84d1ca504f2cecfa24695400f118d1d4
Happykhamissa <elmansourikhamissa@gmail.com>
2023-06-27 03:33:26 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - takes pointer to an integer as a parrameter 
+ * and updates the value it points to to 98
+ * @n: inputed value
+ * Return: n
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
