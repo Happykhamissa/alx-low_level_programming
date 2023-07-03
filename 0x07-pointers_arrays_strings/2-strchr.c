1310430a57eedf9e5ac01424b1326b466fee5196
Happykhamissa <elmansourikhamissa@gmail.com>
2023-07-03 06:27:10 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string.
+ * @c: character to lacate
+ * @s: string
+ * Return: a pointer or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	if (*s == '\0')
+		return (s);
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return ('\0');
+}
