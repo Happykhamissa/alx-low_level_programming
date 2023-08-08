43e163b6183d4b01c5571be2bb068a0575d727aa
Happykhamissa <elmansourikhamissa@gmail.com>
2023-08-08 06:21:25 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * char *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * and contains a copy of the string given as a parameter.
+ * @str: a string
+ *
+ * Return: A pointer to the duplicated string
+ * NULL if memory allocation fails or input string is NULL.
+ */
+
+char *_strdup(char *str)
+{
+
+	unsigned char length = strlen(str);
+
+	char *NewString = (char *)malloc((length + 1) * sizeof(char));
+
+	if (NewString == NULL)
+	{
+		printf("memory not available");
+		return (NULL);
+	}
+	else
+		strcpy(NewString, str);
+	return (NewString);
+}
