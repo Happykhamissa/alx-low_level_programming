84077ddad15bcf95401e9e1d236acf029050b4eb
Happykhamissa <elmansourikhamissa@gmail.com>
2023-08-11 11:16:34 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,35 +12,53 @@
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+
 {
 
-	unsigned int length = n
-	unsigned int i;
-	char *str-nconcat;
-	
-	if (s1 == NULL)
-		s1 = (NULL);
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s2 == NULL)
-		s2 = (NULL);
+	while (s1 && s1[len1])
 
-	for(i = 0; s1[i]; i++)
+		len1++;
+
+	while (s2 && s2[len2])
+
+		len2++;
+
+	if (n < len2)
+
+		s = malloc(sizeof(char) * (len1 + n + 1));
+
+	else
+
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+
+		return (NULL);
+
+	while (i < len1)
+
 	{
-		length++;
-	str-nconcat = (char *)malloc((length + 1) * sizeof(char));
-	
-        if (str-nconcat == NULL)
-        {
-                printf("Memory allocation failed\n");
-                return (NULL);
-        }
-	length = 0;
 
-	for (i = 0; s1[i]; i++)
-		strcpy(str-nconcat, s1);
+		s[i] = s1[i];
 
-	for (i = 0; s2[i] && i < n; i++)
-		strcat(str-nconcat, s2);
+		i++;
+
 	}
-        return (str-nconcat);
+
+
+	while (n < len2 && i < (len1 + n))
+
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
+
 }
