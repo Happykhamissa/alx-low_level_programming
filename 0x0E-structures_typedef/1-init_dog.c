e22fc08cdb333442bd91f227c70a91471fd4fb66
Happykhamissa <elmansourikhamissa@gmail.com>
2023-08-14 11:15:39 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: a struct of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+
+
