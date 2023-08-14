ead0fad5138bf4e4ff5a2064d907d10d4fe3daff
Happykhamissa <elmansourikhamissa@gmail.com>
2023-08-14 09:59:47 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#include "dog.h"
-#include <stdio.h>
+#ifndef _HEADER_
+#define _HEADER_
+
 
 struct dog
 {
@@ -9,3 +10,11 @@ struct dog
 
 };
 
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
