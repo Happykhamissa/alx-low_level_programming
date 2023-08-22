c573563df45eed833b7dbafadd91750082faa6a8
Happykhamissa <elmansourikhamissa@gmail.com>
2023-08-22 16:39:27 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
-	putchar(*s);
-	_puts_recursion(s+1);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
