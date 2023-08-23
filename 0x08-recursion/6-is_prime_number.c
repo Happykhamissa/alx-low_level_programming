35c6bdbb090eef98a9941f04e13b820c98a6bfe8
Happykhamissa <elmansourikhamissa@gmail.com>
2023-08-23 12:01:53 -0700

    6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * is_prime_recursive- recursive
+ * @n: integer number to check
+ * Return: 1 if prime, 0 otherwise
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_recursive(n, n - 1));
+}
+
+/**
+ * is_prime_recursive- recursive
+ * @n: integer number to check
+ * @i: integer number to divise
+ * Return: 1 if prime, 0 otherwise
+*/
+int is_prime_recursive(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime_recursive(n, i - 1));
+}
